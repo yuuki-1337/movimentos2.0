6b8557bedda3b71f3480c4dd18d100dd34972141
Kaue Yuuki Yamamoto <kaueyuukiyamamoto@gmail.com>
2025-08-21 14:49:37 -0300

    Adicionando segundo conteúdo

---
+++ script/script.js
@@ -0,0 +1,17 @@
+function trocarLogo(){
+    let logo = document.getElementById("logo");
+    let largura = window.innerWidth;
+    if (largura <= 768){
+        logo.src = "../img/logo2.png"
+    }
+    else{
+        logo.src = "../img/logo.png"
+    }
+
+}
+window.addEventListener("resize", trocarLogo);
+
+document.getElementById("verMais").addEventListener("click", function(){
+    let proximo = document.getElementById("mainContent");
+    proximo.scrollIntoView({behavior : "smooth"});
+});
