57dfadbeed4153e39ba38f76a3479f1d5425f468
yuuki-1337 <kaueyuukiyamamoto@gmail.com>
2025-08-23 16:06:49 -0300

    Adicionando footer

--- script/script.js
+++ script/script.js
@@ -16,3 +16,30 @@ document.getElementById("verMais").addEventListener("click", function(){
     let proximo = document.getElementById("mainContent");
     proximo.scrollIntoView({behavior : "smooth"});
 });
+
+const instagram = document.getElementById("instagram");
+const instagrams = document.getElementById("instagrams");
+const linkedin = document.getElementById("linkedin");
+const linkedins = document.getElementById("linkedins");
+const redes_sociais = document.getElementById("redes_sociais");
+
+instagram.addEventListener("mouseover", () => {
+    linkedins.classList.remove("active");
+    instagrams.classList.add("active");
+    if(instagrams.style.display == "none"){
+        instagrams.style.display = "flex";
+    }
+});
+
+linkedin.addEventListener("mouseover", () => {
+    instagrams.classList.remove("active");
+    linkedins.classList.add("active");
+    if(linkedins.style.display == "none"){
+        linkedins.style.display = "flex";
+    }
+})
+
+redes_sociais.addEventListener("mouseleave", () => {
+    instagrams.classList.remove("active");
+    linkedins.classList.remove("active");
+});
